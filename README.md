d00a44a0941da07f8677710c4f3c13e268642417
Arin Jain <arinjain349@gmail.com>
2024-02-20 07:45:10 +0530

    [SETUP] created necessary utils and done some testing just to check for any errors if exist

--- README.md
+++ README.md
@@ -81,16 +81,6 @@ We appreciate your interest and look forward to hearing from you!
 
 
 
-
-
-
-
-
-
-
-
-
-
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// cn is a great util which helps us create a dynamic classes inside tailwind 
+// But why we need this why do we both clsx and tailwind merge ?
+// coz there are cases where we could create conflicting classes and instead of writing our own complicated util 
+// to decide which one should we prioritized, we have this nice little util which will gonna help us 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
